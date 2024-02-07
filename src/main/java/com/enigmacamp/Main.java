c41a3d01cad099d759c236d917da9d154914e1db
Muhammad Acla <aclalead@gmail.com>
2024-02-07 09:05:47 +0700

    feat: add cli menu

--- src/main/java/com/enigmacamp/Main.java
+++ src/main/java/com/enigmacamp/Main.java
@@ -1,7 +1,20 @@
 package com.enigmacamp;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("""
+        +----------------------------------------------------+
+        | 1. Mencari Nilai Terkecil dan Terbesar             |
+        | 2. Mencari Elemen/Nilai Minus Terbesar yang Hilang |
+        | 3. Cut The Bamboo                                  |
+        | 4. Mencari Nama yang Ganjil                        |
+        | X. Keluar                                          |
+        +----------------------------------------------------+
+        Pilih Opsi Menu: XX      
+        """);
     }
 }
