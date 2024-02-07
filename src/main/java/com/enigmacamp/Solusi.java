7d3482555fa0b013a35d2285ab54779dc238ff2e
Lely Ruswitasari <lelyr1305@gmail.com>
2024-02-07 09:44:18 +0700

    feat: add find min max

--- src/main/java/com/enigmacamp/Solusi.java
+++ src/main/java/com/enigmacamp/Solusi.java
@@ -9,7 +9,21 @@ interface SolusiInterface {
 public class Solusi implements SolusiInterface{
     @Override
     public void NilaiTerbesarDanTerkecil() {
+        int[] numbers = {6, 2, 4, 10, 5};
 
+        int max = numbers[0];
+        int min = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+
+        System.out.println("max: " + max + " min: " + min);
     }
 
     @Override
