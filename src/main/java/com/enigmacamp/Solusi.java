294c9ab3275d25248e85adc1ce85adb20e1a0a61
Lely Ruswitasari <lelyr1305@gmail.com>
2024-02-07 10:32:23 +0700

    feat add feature: Looking for the Biggest Missing Minus Value

--- src/main/java/com/enigmacamp/Solusi.java
+++ src/main/java/com/enigmacamp/Solusi.java
@@ -6,7 +6,8 @@ interface SolusiInterface {
     void CutTheBamboo();
     void MencariNamaGanjil();
 }
-public class Solusi implements SolusiInterface{
+
+public class Solusi implements SolusiInterface {
     @Override
     public void NilaiTerbesarDanTerkecil() {
         int[] numbers = {6, 2, 4, 10, 5};
@@ -28,6 +29,38 @@ public class Solusi implements SolusiInterface{
 
     @Override
     public void MencariNilaiMinusTerbesarYangHilang() {
+        System.out.println(findMin(new Integer[]{0, 2, 4, -1, -3, -4}));
+    }
+
+    public static Integer findMin(Integer[] numbers) {
+        int missing = 0;
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] > numbers[j]) {
+                    // Tukar elemen jika tidak dalam urutan yang benar
+                    int temp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < 0) {
+                if (i == 0 && numbers[i] != -1) {
+                    missing = -2;
+                } else if (i > 0 && numbers[i] - numbers[i - 1] > 1) {
+                    missing = numbers[i - 1] + 1;
+                }
+            }
+        }
+
+        if (missing == 0 && numbers[numbers.length - 1] < 0) {
+            missing = numbers[numbers.length - 1] + 1;
+        }
+
+        return missing;
 
     }
 
