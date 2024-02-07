f8e67a3855408b5f8511062eafb2f87f0457a1e0
gagaswahyuin <gagaswahyuin@gmail.com>
2024-02-07 09:47:26 +0700

    feat: add cut the bamboo starter

--- src/main/java/com/enigmacamp/Solusi.java
+++ src/main/java/com/enigmacamp/Solusi.java
@@ -1,5 +1,7 @@
 package com.enigmacamp;
 
+import java.util.Scanner;
+
 interface SolusiInterface {
     void NilaiTerbesarDanTerkecil();
     void MencariNilaiMinusTerbesarYangHilang();
@@ -19,7 +21,32 @@ public class Solusi implements SolusiInterface{
 
     @Override
     public void CutTheBamboo() {
+        Scanner scanner = new Scanner(System.in);
 
+        // Input jumlah bambu dan panjang ruasnya
+        System.out.print("Initials (separate with commas): ");
+        String[] ruasInput = scanner.nextLine().split(",");
+        int[] ruasBambu = new int[ruasInput.length];
+        for (int i = 0; i < ruasInput.length; i++) {
+            ruasBambu[i] = Integer.parseInt(ruasInput[i]);
+        }
+
+        // Input jumlah siklus pemotongan
+        System.out.print("Number of cutting cycles: ");
+        int jumlahSiklus = scanner.nextInt();
+
+        // Simulasi pemotongan ruas bambu
+        for (int siklus = 1; siklus <= jumlahSiklus; siklus++) {
+            for (int i = 0; i < ruasBambu.length; i++) {
+                ruasBambu[i]--;
+            }
+            System.out.print("\nCycle Cuts " + siklus + ":\n");
+            for (int i = 0; i < ruasBambu.length; i++) {
+                System.out.print(ruasBambu[i] + " ");
+            }
+        }
+
+        System.out.println();
     }
 
     @Override
